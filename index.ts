1300be820d624345ea91fa4d8e5b07a833bd9be3
Denis Carriere <carriere.denis@gmail.com>
2023-06-27 16:23:51 -0400

    Add headers

--- index.ts
+++ index.ts
@@ -5,11 +5,18 @@ import { nonce, send } from "./src/actions.js";
 
 export default {
     port: PORT,
+    cors: true,
+    Headers: {
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type'
+    },
     async fetch(request: Request) {
         const session = createSession();
         const url = new URL(request.url);
         if ( request.method == "GET" ) {
-            if ( url.pathname == "/" ) return new Response(banner());
+            if ( url.pathname == "/" ) return toText(banner());
             const info = await session.client.v1.chain.get_info();
             return toJSON(info.toJSON());
         }
@@ -21,15 +28,18 @@ export default {
             const response = await session.transact({actions}, {})
             return toJSON(response.response);
         }
+        return toText("OK");
     },
 };
 
 export function toJSON(data: any, status = 200) {
     return new Response(JSON.stringify(data), {status, headers: {
-        'Content-Type': 'application/json',
-         // CORS
-        'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': '*',
+        'Content-Type': 'application/json'
+    }});
+}
+
+export function toText(data: any, status = 200) {
+    return new Response(data, {status, headers: {
+        'Content-Type': 'text/plain;charset=utf-8',
     }});
 }
